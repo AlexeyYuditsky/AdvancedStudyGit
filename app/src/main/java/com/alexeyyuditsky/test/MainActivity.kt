a05efc0d0b87e9a65cb2189c68290788b1be6fe8
Alexey Yuditsky <alexeyyuditsky@gmail.com>
2021-12-08 18:02:50 +0700

    android project !!!

---
+++ app/src/main/java/com/alexeyyuditsky/test/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.alexeyyuditsky.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alexeyyuditsky.test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.button.setOnClickListener {  }
+    }
+
+}
